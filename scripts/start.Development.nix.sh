f0aeef1627c302a1e9b747eb59b3815a4613bd14
brunorafaelcosta <brunorafael.costa@outlook.com>
2020-04-18 20:24:50 +0100

    linux docker-compose setup

--- scripts/start.Development.nix.sh
+++ scripts/start.Development.nix.sh
@@ -2,4 +2,4 @@
 
 cd ../src/
 
-docker-compose -f "docker-compose.yml" -f "docker-compose.override.yml" --env-file=".env" up -d --build
+docker-compose -f "docker-compose.yml" -f "docker-compose.override.yml" -f "docker-compose.linux.override.yml" --env-file=".env" up -d --build
--- scripts/start.Development.win.ps1
+++ scripts/start.Development.win.ps1
@@ -1,1 +1,3 @@
+cd ../src/
+
 docker-compose -f "docker-compose.yml" -f "docker-compose.override.yml" --env-file=".env" up -d --build
